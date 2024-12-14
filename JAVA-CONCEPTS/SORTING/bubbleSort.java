d5b585c316e53028c37c3419a879178e016312de
HarshMane2604 <maneharsh72@gmail.com>
2024-12-14 12:47:34 +0530

    Inbuilt function for Sorting in Java

--- JAVA-CONCEPTS/SORTING/bubbleSort.java
+++ JAVA-CONCEPTS/SORTING/bubbleSort.java
@@ -1,7 +1,7 @@
 public class bubbleSort{
     public static void bubbleSort(int arr[]){
         for (int i = 0; i < (arr.length - 1); i++){
-            for(int j = 0; j < (arr.length- i); j++){
+            for(int j = 0; j < (arr.length- i -1); j++){
                 if(arr[j] > arr[j+1]){
                     int a;
                     a = arr[j];
