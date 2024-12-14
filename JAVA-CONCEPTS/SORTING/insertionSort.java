8540d79c9e3b5da234eb37fa2ffb02346df2de06
HarshMane2604 <maneharsh72@gmail.com>
2024-12-14 12:00:58 +0530

    INsertion sort coding part

--- JAVA-CONCEPTS/SORTING/insertionSort.java
+++ JAVA-CONCEPTS/SORTING/insertionSort.java
@@ -1,14 +1,27 @@
 public class insertionSort{
     public static void insertionSort(int arr[]){
-        for(int i = 1; i < arr.length - 1; i++){
-            int curr = i;
+        // for(int i = 1; i < arr.length - 1; i++){
+        //     int curr = i;
+        //     int prev = i - 1;
+        //     if(arr[curr] < arr[prev]){
+        //         int temp = arr[curr];
+        //         arr[curr] = arr[prev];
+        //         arr[prev] = temp;
+        //     }
+        // }
+
+        for(int i = 1; i < arr.length; i++){
+            int curr = 1;
             int prev = i - 1;
-            if(arr[curr] < arr[prev]){
-                int temp = arr[curr];
-                arr[curr] = arr[prev];
-                arr[prev] = temp;
+            // Finiding out the correct pos to insert
+            while(prev >= 0 && arr[prev] > arr[curr]){
+                arr[prev + 1] = arr[prev];
+                prev--;
             }
-        }
+            //INSERTION 
+            arr[prev+1] = arr[curr];
+
+        } 
     }
     public static void printArr(int arr[]){
         for(int i = 0; i < arr.length; i++){
