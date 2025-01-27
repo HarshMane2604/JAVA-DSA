94f027d341211f7cafef2e99067be29cf1770c30
HarshMane2604 <maneharsh72@gmail.com>
2025-01-27 12:38:16 +0530

    Learned about concatenation in string

---
+++ JAVA-CONCEPTS/STRING/StringConcatenation.java
@@ -0,0 +1,10 @@
+package STRING;
+import java.util.*;
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName = "Harsh";
+        String lastName = "Mane";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName + " is this");
+    }
+}
