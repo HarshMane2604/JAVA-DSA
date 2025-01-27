149dcc6bbc33cec10a049439a3b8e328c483a143
HarshMane2604 <maneharsh72@gmail.com>
2025-01-27 12:34:24 +0530

    Learned to get the length of string

--- JAVA-CONCEPTS/STRING/StringLength.java
+++ JAVA-CONCEPTS/STRING/StringLength.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
 public class StringLength {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+        System.out.println(line.length());
+        //In array length is the property but in string length is function
+        //so it is necessary to add "()" after length
+    }
 }
