5eb8ec17be94b9a1fa2fa26b3b26bb7f07c928b4
HarshMane2604 <maneharsh72@gmail.com>
2025-01-27 17:03:09 +0530

    Solved a question

---
+++ JAVA-CONCEPTS/STRING/findShortestPath.java
@@ -0,0 +1,30 @@
+package STRING;
+import java.util.*;
+public class findShortestPath {
+    public static float shortestDist(String path){
+        int x = 0 , y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if(dir == 'S'){
+                y--;
+            }else if(dir == 'N'){
+                y++;
+            }else if(dir == 'W'){
+                x--;
+            }
+            else{
+                x++;
+            }
+        }
+        int x2 = x * x;
+        System.out.println(x2);
+        int y2 = x * x;
+        System.out.println(y2);
+        return (float)Math.sqrt(x2 + y2);
+    }
+    public static void main(String[] args) {
+        String path = "EWNS";
+        System.out.println(shortestDist(path));
+
+    }
+}
