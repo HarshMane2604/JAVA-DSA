a6d0b5ebb2655a5c6149c20af70f1dfa094c2061
HarshMane2604 <maneharsh72@gmail.com>
2025-01-29 14:52:22 +0530

    Question of camelCase solved

--- PRACTICE/practice.java
+++ PRACTICE/practice.java
@@ -1,22 +1,59 @@
+
+// QUESTION OF GCD
+
+// import java.util.*;
+// public class practice {
+//     public static int Division(int a, int b){
+//         if(a == 0 || b == 0){
+//             return 0;
+//         }else{
+//             while (b != 0) {
+//                 int temp = b;
+//                 b = a % b;
+//                 a = temp;
+//             }
+//         }
+//         return a;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         System.out.println(Division(a, b));
+
+//     }
+// }
+
+// QUESTION TO CONVERT to CAMELCASE
 import java.util.*;
+
+import javax.swing.plaf.synth.SynthStyle;
+
 public class practice {
-    public static int Division(int a, int b){
-        if(a == 0 || b == 0){
-            return 0;
-        }else{
-            while (b != 0) {
-                int temp = b;
-                b = a % b;
-                a = temp;
+    public static String camelCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        boolean capital = false;
+        for (char ch : str.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                if (str.length() == 0) {
+                    sb.append(Character.toLowerCase(ch));
+                } else if (capital) {
+                    sb.append(Character.toUpperCase(ch));
+                } else {
+                    sb.append(Character.toLowerCase(ch));
+                }
+                capital = false;
+            }else{
+                capital = true;
             }
+            
         }
-        return a;
+        return sb.toString();
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(Division(a, b));
-        
+        String str = sc.nextLine();
+        System.out.println(camelCase(str));
     }
 }
