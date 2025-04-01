2f1a8ccca0c916ae624748842201a2c0c1a4a6cd
HarshMane2604 <maneharsh72@gmail.com>
2025-04-02 00:01:03 +0530

    Optimized way of the code

--- PRACTICE/String_Practice/FirstNonRepeating.java
+++ PRACTICE/String_Practice/FirstNonRepeating.java
@@ -1,19 +1,30 @@
 import java.util.*;
 public class FirstNonRepeating{
     public static char nonRepeating(String str){
-        for(int i = 0; i < str.length(); i++){
-            boolean found = false;
+        // for(int i = 0; i < str.length(); i++){
+        //     boolean found = false;
 
-            for(int j = 0; j < str.length(); j++){
-                if(i != j && str.charAt(i) == str.charAt(j)){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                return str.charAt(i);
+        //     for(int j = 0; j < str.length(); j++){
+        //         if(i != j && str.charAt(i) == str.charAt(j)){
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         return str.charAt(i);
+        //     }
+        // }
+
+        int [] freq = new int[26];
+        for (char i : str.toCharArray()) {
+            freq[i - 'a']++;
+        }
+        for (char i : str.toCharArray()) {
+            if(freq[i - 'a'] == 1){
+                return i;
             }
         }
+ 
         return '$';
     }
     public static void main(String[] args){
